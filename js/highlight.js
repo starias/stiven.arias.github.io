a2d728195ab8f67c269249aa22ff362624c67cc3
Stiven Arias <starias97@hotmail.com>
2025-01-31 21:27:59 +0200

    FIX: bug with the section indexes

--- js/highlight.js
+++ js/highlight.js
@@ -2,14 +2,24 @@ const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll(".navigation ul li a");
 const nSections = 3
 
+let sectionTops = []
+
+sections.forEach((section, index) => {
+    sectionTops[index] = section.offsetTop
+});
+
 document.addEventListener("scroll", () => {
     let currentSection = "";
     
     // Iterate through each section to find which one is in view
-    sections.forEach(section => {
+    sections.forEach((section, index) => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
-        if (scrollY >= sectionTop - sectionHeight / nSections) {
+
+        if (index == nSections - 1 && scrollY >= sectionTops[index]) {
+            currentSection = section.getAttribute("id");
+        }
+        else if (scrollY >= sectionTops[index] && scrollY <= sectionTops[index+1]) {
             currentSection = section.getAttribute("id");
         }
     });
